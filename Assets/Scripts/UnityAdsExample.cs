6eb5a70cc4874bd41f24c83eccf7cbb77e41636c
AlexBonilla57 <bonilla.alex57@gmail.com>
2017-03-13 19:55:49 -0700

    DesktopCommit1

---
+++ Assets/Scripts/UnityAdsExample.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+#if UNITY_ADS
+using UnityEngine.Advertisements;
+#endif
+
+public class UnityAdsExample : MonoBehaviour
+{
+    void Start()
+    {
+#if UNITY_ADS
+        Advertisement.Initialize("1344773", true);
+        
+        StartCoroutine(ShowAdWhenReady());
+#endif
+    }
+
+#if UNITY_ADS
+    System.Collections.IEnumerator ShowAdWhenReady()
+    {
+        while (!Advertisement.IsReady())
+            yield return null;
+
+        Advertisement.Show();
+    }
+#endif
+
+}
